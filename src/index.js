ee796031642ef77c72102ae841f55bb8a64c1916
edua152030 <eduardo.vaz.faria@gmail.com>
2023-08-24 08:37:36 -0300

    termino

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { v4 as uuidv4 } from 'uuid'
-import bcrypt from 'bcrypt '
+import bcrypt from 'bcrypt'
 
 const app = express()
 const porta = 3333
@@ -10,45 +10,141 @@ const tasks = []
 
 app.use(express.json())
 
-app.post("/createUser", (request, response) =>{
-    //pegando as variaveis do body
+app.post("/createUser", async (request, response) => {
+
     const { name, email, pass } = request.body
 
-    //percorrendo o array para validar se o email esta = body.email
     const emailExistente = users.find(user => user.email === email)
-    
-    //alertando se ja tem email cadastrado
-    if (emailExistente){
+
+    if (emailExistente) {
         return response.status(400).json({
             message: 'email ja existe no cadastro'
         })
     }
 
-    //criando objeto para o push
+    const passEncripted = await bcrypt.hash(pass, 9)
+
     const newUser = {
         id: uuidv4(),
         name,
         email,
-        pass
+        pass: passEncripted
     }
 
-    //verificando se a senha é valida
-    const passwordFail = users.find(user => user.pass === pass)
-    
-    //inserindo no array o objeto criado
     users.push(newUser)
-    
-    //resposta se deu tudo ok
+
     response.status(201).json({
         message: 'usuario cadastrado', newUser
     })
 })
 
-app.get("/listUsers", (request, response) => {
-    const user =  users.forEach()
-    return response.status(200).json({
-        user
+app.post("/loginUser", async (request, response) => {
+    const { email, pass } = request.body
+
+    const user = users.find(user => user.email === email)
+
+    if (!user) {
+        return response.status(404).json({
+            message: 'usuario nao localizado'
+        })
+    }
+
+    const passIdent = await bcrypt.compare(pass, user.pass)
+
+    if (!passIdent) {
+        return response.status(400).json({
+            message: 'informacoes nao sao validas'
+        })
+    }
+
+    response.status(200).json({
+        message: 'usuario logado', id: user.id
     })
 })
 
-app.listen(porta, () => {console.log(`servidor iniciado na porta ${porta}`)})
+app.get("/listUsers", (request, response) => {
+    response.status(200).json(users);
+});
+
+app.post("/createTask", (request, response) => {
+    const { title, description, userId} = request.body
+    const user = users.find(user => user.id === userId)
+
+    if(!user){
+        response.status(404).json({
+            message: 'usuario nao cadastrado'
+        })
+    }
+
+    const newTask = {
+        id: uuidv4(),
+        title, 
+        description,
+        userId
+    }
+    tasks.push(newTask)
+
+    response.status(201).json({
+        messsage: 'tarefa cadastrada', task: newTask
+    })
+
+})
+
+app.get("/listTask/:userId", (request, response) => {
+
+    const { userId } = request.params
+
+    const user = users.find(user => user.id === userId)
+
+    if(!user){
+        response.status(404).json({
+            message: "usuario nao encontrado"
+        })
+    }
+
+    const filtred = tasks.filter(task => task.userId === userId)
+
+    response.status(200).json({
+     filtred
+    })
+});
+
+app.put("/updateTask/:id", (request, response) => {
+    const { id } = request.params
+    const {title, description } = request.body
+    const task = tasks.find(task => task.id === id)
+
+    if(!task){
+        response.status(404).json({
+            message: "mensagem nao localizada"
+        })
+    }
+
+    task.title = title || task.title;
+    task.description = description || task.description;
+
+    response.status(200).json({
+        message: "tarefa atualizada ",
+        updateTask: task
+    })
+    
+})
+
+app.delete("/deleteTask/:id", (request, response) => {
+    const { id } = request.params
+    const deleted = tasks.findIndex(task => tasks.id === id)
+
+    if(!deleted === -1){
+        return response.status(404).json({
+            message:"id nao localizado"
+        })
+    }
+    tasks.splice(deleted, 1)
+    response.status(200).json({
+        message: "tarefa deletada", deleted
+    })
+})
+
+
+
+app.listen(porta, () => { console.log(`servidor iniciado na porta ${porta}`) })
